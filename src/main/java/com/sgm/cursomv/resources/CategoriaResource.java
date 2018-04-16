77efed2530fbd33b4d03a816abdf2413c18ecf24
sgmmachado <sgmgenari@gmail.com>
2018-04-16 15:13:31 -0300

    Testado 1ª classe categoria 10%

--- src/main/java/com/sgm/cursomv/resources/CategoriaResource.java
+++ src/main/java/com/sgm/cursomv/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.sgm.cursomv.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sgm.cursomv.domain.Categoria;
+
 
 @RestController
 @RequestMapping(value="/categorias") 
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está Funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1=new Categoria(null,"Informatica");
+		Categoria cat2=new Categoria(null,"Escritório");
+		List<Categoria> lista= new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
